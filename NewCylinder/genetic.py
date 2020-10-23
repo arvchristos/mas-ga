5d167fcb2c14120c15316c578c76c86af126af88
Emmanouil Bagakis <manos.bagakis@gmail.com>
2020-10-23 12:18:38 +0200

    writing output, plot in txt/png

--- NewCylinder/genetic.py
+++ NewCylinder/genetic.py
@@ -1,11 +1,14 @@
 import numpy as np
-import random
+import random, time, math, pickle
 from ga_algorithm import geneticalgorithm as ga
 from Cylinder import Cylinder
-import time
+import matplotlib.pyplot as plt
 ### Init parameters ###
-r_c, r_s, = 0.8, 1.6
-r_obs = r_c
+r_c, r_obs = 0.8000, 0.8000 # The same 
+r_s = 1.6
+N = 40
+
+
 limits=np.array([[0,1]]*1)
 
 
@@ -25,11 +28,11 @@ def eval(value):
 	return value
 
 
-rules={'max_num_iteration': 20,
+rules={'max_num_iteration': 10,
 	   'population_size':20,
-       'mutation_probability':0.5,
-	   'elit_ratio': 0.01,
-	   'crossover_probability': 0.5,
+       'mutation_probability':0.8,
+	   'elit_ratio': 0.1,
+	   'crossover_probability': 0.4,
 	   'parents_portion': 0.1,
 	   'crossover_type':'uniform',
 	   'max_iteration_without_improv':None}	
@@ -42,13 +45,13 @@ model=ga(
 		 variable_boundaries=limits,
 		 algorithm_parameters=rules)
 
-
+######################################################################
 #run the GAlgorithm
 time1 = time.time()
 report, champion = model.run()
 time_elasped = time.time() - time1
 
-	
+######################################################################	
 #print fetched data
 print("\n time_elasped = ", time_elasped)
 print("printing error convergence...")
@@ -62,13 +65,40 @@ print("max error = ", max_)
 print("and CN = ", CN)
 _, _, ezmas, _ = eval_mas(champion.get('variable'))
 print("and ez_MAS value", ezmas)
+######################################################################
+### write data on a file ###
+with open("cylinder%s.txt" %str(r_c)[0:5], "w") as fin:
+	fin.write("Problem parameters:")
+	fin.write("\nr_cylinder = %s " %r_c + "r_obs = %s " %r_obs + "r_s = %s" %r_s)
+	fin.write("\nN = %s" %N)
+	fin.write("\nResults:")
+	fin.write("\nBest r_aux value was found to be %s " %champion.get('variable')+
+			  "in %s minutes " %(time_elasped//60) +
+			  "and %s seconds" %str(time_elasped%60)[0:4])
+	fin.write("\n \n")
+	fin.write("mean(error) = %s " %mean +"max(error) = %s " %max_ +
+		      "CN = %s" %CN)
+	fin.write("\nprinting the best r_aux per generation")
+	for person in report:
+		fin.write("\n"+"%s" %person)
+	
 
-#time + max,mean error progress
-#EZ_maz for champion	
-
-#TO-DO 
-#write and plot to a file
-#
-#
+f = open("EZ_MAS%s.txt" %str(r_c)[0:5], 'wb')
+pickle.dump(ezmas, f)
+f.close()
+### to load ###
+#f = open('store.pckl', 'rb')
+#obj = pickle.load(f)
+#f.close()
+#######################################################################
+### plot ###
+plt.plot(report)
+plt.xlabel('Generations')
+plt.ylabel('Max Error')
+plt.title('Error Convergence cylinder = %s' %(str(r_c)[0:5]))
+plt.savefig('cylpi%s.png' % str(r_c)[0:5], dpi=400, bbox_inches='tight')
+plt.show()
+#######################################################################
+### run for different params
 
 
