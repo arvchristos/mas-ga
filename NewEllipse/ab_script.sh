7defca900e7f255441d67c1841c844a6090a651f
manosbagakis <manos.bagakis@gmail.com>
2020-11-21 10:58:11 +0000

    fixed even N

--- NewEllipse/ab_script.sh
+++ NewEllipse/ab_script.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
-python3 genetic.py 4 4 20 30 45 &&
-python3 genetic.py 4 4 20 40 45 &&
-python3 genetic.py 4 4 20 50 45 &&
-python3 genetic.py 4 4 20 60 45 &&
-python3 genetic.py 4 4 20 70 45 
+python3 genetic.py 4 4 20 20 46 &&
+python3 genetic.py 4 4 20 30 46 &&
+python3 genetic.py 4 4 20 40 46 &&
+python3 genetic.py 4 4 20 50 46 &&
+python3 genetic.py 4 4 20 60 46 &&
+python3 genetic.py 4 4 20 70 46 &&
+python3 genetic.py 4 4 30 20 46 &&
+python3 genetic.py 4 4 40 20 46 &&
+python3 genetic.py 4 4 50 20 46 &&
+python3 genetic.py 4 4 60 20 46 &&
+python3 genetic.py 4 4 70 20 46 
