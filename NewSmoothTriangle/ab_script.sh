d233d360ca1e6507dfa29293681f01d82145b39f
manosbagakis <manos.bagakis@gmail.com>
2020-12-01 09:21:26 +0000

    last changes

--- NewSmoothTriangle/ab_script.sh
+++ NewSmoothTriangle/ab_script.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/bash
-python3 genetic.py 0 5 20 20 51 &&
-python3 genetic.py 1 5 20 20 51 &&
-python3 genetic.py 2 5 20 20 51 &&
-python3 genetic.py 3 5 20 20 51 &&
-python3 genetic.py 4 5 20 20 51 &&
-python3 genetic.py 5 5 20 20 51 &&
-python3 genetic.py 6 5 20 20 51 &&
-python3 genetic.py 7 5 20 20 51
-
+python3 genetic.py 0 5 10 10 48
--- NewSmoothTriangle/single_run.py
+++ NewSmoothTriangle/single_run.py
@@ -1,12 +1,13 @@
 from SmoothTriangle import SmoothTriangle
 import sys
-
+import numpy as np
 gamma = 0.05
 N = 60 
 c_aux = 0.9
 k = 1
 c_obs = 1
 
-schema = SmoothTriangle(N=N, gamma=gamma, k=k, c_aux=c_aux, c_obs=c_obs)
-print(schema.mas())
+for it in np.linspace(0.001, 0.999 ,50): 
+	schema = SmoothTriangle(N=N, gamma=gamma, k=k, c_aux=it, c_obs=c_obs)
+	print(schema.mas(),it)
 
