94a267f13cdf2dc9e96fbb77be63fabeaa05018f
manosbagakis <manos.bagakis@gmail.com>
2021-01-05 12:00:07 +0000

    added 20-150 test

--- NewEllipse/ab_script.sh
+++ NewEllipse/ab_script.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/bash
-python3 genetic.py 4 4 20 20 46 &&
-python3 genetic.py 4 4 20 30 46 &&
-python3 genetic.py 4 4 20 40 46 &&
-python3 genetic.py 4 4 20 50 46 &&
-python3 genetic.py 4 4 20 60 46 &&
-python3 genetic.py 4 4 20 70 46 &&
-python3 genetic.py 4 4 30 20 46 &&
-python3 genetic.py 4 4 40 20 46 &&
-python3 genetic.py 4 4 50 20 46 &&
-python3 genetic.py 4 4 60 20 46 &&
-python3 genetic.py 4 4 70 20 46 
+python3 genetic.py 4 4 20 150 46
